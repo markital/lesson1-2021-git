de0d651be694278e9c0c71000483b63ad86a7d44
markital <markital@ukr.net>
2021-06-22 18:49:46 +0300

    Я зробив зміни 1

---
+++ main.js
@@ -0,0 +1,96 @@
+//1.Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//let name ="hello,owu,com,ua,1,10,-999,123,3.14,2.7,16,true,false" 
+
+//2.Вивести кожну змінну за допомогою: console.log , alert, document.write
+//alert('hello,owu,com,ua,1,10,-999,123,3.14,2.7,16,true,false')
+//let name="hello, owu, com, ua, 1, 10, -999, 123, 3.14, 2.7, 16, true, false "
+//console.log(name)
+//document.write('hello, owu, com, ua, 1, 10, -999, 123, 3.14, 2.7, 16, true, false');
+
+//3.Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//let name ="hello, owu, com, ua, 1, 10, -999, 123, 3.14, 2.7, 16, true, false"
+//name ="hi, go, nice, like, 5, 15, -60, 414, 12.5, 3.3, 23, Ok, super"
+
+//4.Вивести кожну змінну за допомогою: console.log , alert, document.write
+//let name = "hi, go, nice, like, 5, 15, -60, 414, 12.5, 3.3, 23, Ok"
+//console.log(name)
+//alert('hi, go, nice, like, 5, 15, -60, 414, 12.5, 3.3, 23, Ok')
+//document.write('hi, go, nice, like, 5, 15, -60, 414, 12.5, 3.3, 23, Ok')
+
+//5.Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. Зконкатенувати їх в одну змінну person.
+//let person = {
+ //firstName:"Markiyan",
+ //middleName:"Volodymyrovuch",
+ //lastName:"Seredynskyy"}
+
+//6.За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+ //Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+ //let firstName=prompt('What is your name?');
+ //let middleName=prompt('What is your middleName?');
+ //let age=+prompt('How old are you?');
+   //firstName==='Markiyan', middleName==='Volodymyrovuch',
+  //age==='43'
+   //alert('Вітаю Markiyan Volodymyrovuch.Тобі 43 роки')
+
+  //7. За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  // let a = 100; let b = '100'; let c = true;
+  //let a=100
+  //console.log(typeof a)
+  //let b ='100'
+  //console.log(typeof b)
+  //let c=true
+  //console.log(typeof c)
+
+  //8.Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+   //  В однакових виразаї не використовувати однакові оператори!!!
+  //5 < 6 -> true
+  //5 > 6 -> false
+  //5 >= 6 -> false
+  //5 = 6 -> false
+  //10 === 10 -> true
+  //10 ==10 -> true
+  //10 > 10 -> false
+  //10 >= 10 -> false
+  //10 != 10 -> false
+  //123 >= '123' -> false
+  //123 === '123' -> true
+  //Додатково:
+//- Подивіться на наступні вирази і спробуйте вгадати яким буде вивід в консоль. Перевірте себе.
+//console.log(132 > 100 && 45 < 12 );
+//console.log(34 > 33 && 23 < 90 );
+//console.log(99 > 100 && 45 > 12 );
+//console.log(132 > 100 || 45 < 12 );
+//console.log(111 > 11 || 45 < 111 );
+//console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+//console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+//console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+//console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+//console.log(!!'-1');
+//console.log(!!-1);
+//console.log(!!'0');
+//console.log(!!'null');
+//console.log(!!'undefined');
+//console.log(!!(3/'owu'));
+//console.log((111 > 11 || 45 < 111) ||  !!'0');
+//console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+
+
+
+  
+  
+
+
+
+ 
+
+
+
+//'owu','com', 'ua', '1', '10', '-999', '123','3.14', '2.7','16','true','false')
+
+
+
+
+
+
+
+
